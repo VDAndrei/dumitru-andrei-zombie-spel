20c6c7f6db6b06fda00dde9f6760a5ee27f99133
VDAndrei <dumitru-andrei.varzaru.gbg22@amerikanskagymnasiet.se>
2023-04-25 11:24:56 +0200

    shooting

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-var player = { x: 100, y: 100, size: 20, xSpeed: 5, ySpeed: 0 };
+var player = { x: 100, y: 100, size: 40, xSpeed: 5, ySpeed: 0 };
+let bullet = [];
 
 let cooldown = 0;
 
-function update() {
+let bulletCooldown = 0;
 
+function gun() {
+    if (mouse.left && bulletCooldown === 0) {
+        bullet.push(
+            { x: player.x, y: player.y, size: 10, xSpeed: 7 * (mouse.x - player.x) / distance(player, mouse), ySpeed: 7 * (mouse.y - player.y) / distance(player, mouse) }
+        )
+        bulletCooldown = 10;
+    }
+    if (bulletCooldown > 0) {
+        bulletCooldown--;
+    }
+    console.log(bullet)
+}
+
+function update() {
+    gun();
     clearScreen()
-    rectangle(player.x, player.y, player.size, player.size, "green");
+    rectangle(player.x - player.size / 2, player.y - player.size / 2, player.size, player.size, "green");
     player.xSpeed = 0
     player.ySpeed = 0
     if (keyboard.w) {
@@ -24,11 +40,34 @@ function update() {
         player.x = mouse.x
         player.y = mouse.y
         console.log("yes")
-        cooldown = 150
+        cooldown = 150 //150 in the future
     }
     if (cooldown > 0) {
         cooldown--;
     }
+    if (player.x + player.size / 2 >= innerWidth) {
+        player.x = innerWidth - player.size / 2
+        player.xSpeed = -1
+    } if (player.x - player.size / 2 <= 0) {
+        player.x = player.size / 2
+        player.xSpeed = 1
+    } if (player.y + player.size / 2 >= innerHeight) {
+        player.y = innerHeight - player.size / 2
+        player.ySpeed = -1
+    } if (player.y - player.size / 2 <= 0) {
+        player.y = player.size / 2
+        player.ySpeed = 1
+    }
+
+    for (var i = 0; i < bullet.length; i++) {
+        rectangle(bullet[i].x, bullet[i].y, bullet[i].size, bullet[i].size, "red")
+        bullet[i].x += bullet[i].xSpeed;
+        bullet[i].y += bullet[i].ySpeed;
+        console.log("bullet")
+    }
+
+
+
     player.x += player.xSpeed;
     player.y += player.ySpeed;
 
